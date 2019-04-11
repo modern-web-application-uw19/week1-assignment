5657d6d937125df9c85930864cc1f53d4c4f1f13
Laura_MS <lmayers3@uw.edu>
2019-04-11 18:45:15 -0400

    created class

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,14 @@
 // Define class here
+class HTMLElements {
+    constructor(tag, content) {
+        this.tag = tag;
+        this.content = content;
+    }
+
+    render(tag, content) {
+        return `<${tag}>${content}</${tag}>`;
+    }
+}
 
 // Export class here
 module.exports = {};
