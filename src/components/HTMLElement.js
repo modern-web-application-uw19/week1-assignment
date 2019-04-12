03d4458bfc445714a323d8b254ee55cb9988f4d3
Laura_MS <lmayers3@uw.edu>
2019-04-11 21:30:53 -0400

    fixing some issues

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,14 +1,19 @@
 // Define class here
-class HTMLElements {
+class HTMLElement {
     constructor(tag, content) {
         this.tag = tag;
         this.content = content;
     }
 
     render(tag, content) {
-        return `<${tag}>${content}</${tag}>`;
+        return `<${this.tag}>${this.content}</${this.tag}>`;
     }
 }
 
+const lovelaceQuote = new HTMLElement('p', 'I am never so happy as when I am really engaged in good earnest...');
+
+console.log(lovelaceQuote.render());
+// prints "<p>I am never so happy as when I am really engaged in good earnest...</p>"
+
 // Export class here
-module.exports = {};
+module.exports = HTMLElement;
