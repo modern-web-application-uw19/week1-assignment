e6421255d3d5a7493894f7e034fe3aa3e552650c
Alison Zerbe <alisonkzerbe@gmail.com>
2019-04-13 15:48:40 -0700

    uses fat arrow instead of assigning this to instance

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -2,17 +2,17 @@ class Timer {
   constructor(seconds) {
     this.seconds = seconds;
   }
+
   start() {
-    let instance;
-    const timerInterval = setInterval(function () {
+    const timerInterval = setInterval((instance) => {
       if (instance.seconds === 0) {
         clearInterval(timerInterval);
       }
+  
       console.log(instance.seconds);
       instance.seconds -= 1;
     }, 1000);
-  }
+  };
 }
-
 
 export default Timer;
