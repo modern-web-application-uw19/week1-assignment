f458093c30e65885d0cba6866f03793ec1e57495
Laura_MS <lmayers3@uw.edu>
2019-04-12 20:09:52 -0400

    defined class

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,17 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
+class DivElement extends HTMLElement {
+    constructor(content) {
+        super("div", content);
+        this.content = content;
+    }
+}
+
+const andIThinkToMyself = new DivElement('What a wonderful world');
+
+console.log(andIThinkToMyself.render());
+// prints "<div>What a wonderful world</div>"
 
 // Export class here
 module.exports = {};
