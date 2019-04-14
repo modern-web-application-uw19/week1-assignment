dc4194a8d15cc1c6be1f56c1f7f5165b7ed8275c
Shahab <shahab13@uw.edu>
2019-04-13 22:22:07 -0700

    element practice

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,20 @@
 const HTMLElement = require('./HTMLElement');
-
+//import HTMLElement from './HTMLElement'
 // Define class here
+class DivElement extends HTMLElement {
+
+    constructor(tag,content){
+
+        super(tag,content);
+    }
+
+
+}
 
 // Export class here
-module.exports = {};
+module.exports = {DivElement};
+
+const xx = new DivElement('h1','What a wonderful world');
+
+//console.log(andIThinkToMyself.render());
+console.log(xx.render());
