644a3bc38c0877c46b9f05174b497d5a2e0c8cc7
Mahsa Ataei <mataei@uw.edu>
2019-04-14 13:29:19 -0700

    week1 assignment

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,22 @@
 // Define class here
+class HTMLElement {
+  constructor(tag, content) {
+    this.tag = tag;
+    this.content = content;
+  }
+
+  render() {
+    if (this.content !== undefined) {
+      return `<${this.tag}>${this.content}</${this.tag}>`;
+    }
+    return `<div>${this.tag}</div>`;
+  }
+}
+
+// Test class here
+const lovelaveQuote = new HTMLElement('p', 'I am never so happy as when I am really engaged in good earnest...');
+console.log(lovelaveQuote.render());
+
 
 // Export class here
-module.exports = {};
+module.exports = HTMLElement;
