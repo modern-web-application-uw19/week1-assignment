a1c106a0e3adacf0ee46fc9a1c08965215906971
Wai Lam Wong <wlmwng@gmail.com>
2019-04-14 19:19:53 -0500

    remove comment

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -3,7 +3,6 @@ const HTMLElement = require('./HTMLElement');
 // Define class here
 class DivElement extends HTMLElement {
   constructor(content) {
-    // passed to the parent Book constructor
     super();
     this.tag = 'div';
     this.content = content;
