be4a90211eb12dba0362151c2d9953636acc1e00
Wai Lam Wong <wlmwng@gmail.com>
2019-04-14 19:17:03 -0500

    passes tests

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,14 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(content) {
+    // passed to the parent Book constructor
+    super();
+    this.tag = 'div';
+    this.content = content;
+  }
+}
 
 // Export class here
-module.exports = {};
+module.exports = DivElement;
