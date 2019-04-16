70f89b3cb620775274dc58270c0205997ad961cb
NETID\oieeri26 <oieeri26@uw.edu>
2019-04-16 14:05:44 -0700

    updated HTMLElement prototype

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -2,5 +2,14 @@ const HTMLElement = require('./HTMLElement');
 
 // Define class here
 
+class DivElement {
+  constructor(content) {
+    this.tag = 'div';
+    this.content = content;
+  }
+}
+
+// DivElement.prototype
+
 // Export class here
-module.exports = {};
+module.exports = DivElement;
--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -4,9 +4,14 @@ class HTMLElement {
   constructor(tag, content) {
     this.tag = tag;
     this.content = content;
-    this.render = `<${tag}>${content}</${tag}>`;
+    // this.render = `<${tag}>${content}</${tag}>`;
   }
 }
+
+HTMLElement.prototype.render = function () {
+  const instance = this;
+  return `<${instance.tag}>${instance.content}</${instance.tag}>`;
+};
 
 // Export class here
 module.exports = HTMLElement;
