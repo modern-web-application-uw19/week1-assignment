6c4e5ca23d555807cb727c47f9423dc590130583
Shawn Hopkins <shopkins@seagen.com>
2019-04-16 14:59:41 -0700

    eol

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,15 @@
 // Define class here
 
+class HTMLElement {
+    constructor(tag,content){
+      this.tag = tag;
+      this.content=content;
+    }
+    const render = function ( ) {
+      return `<{this.tag}>{this.content}</{this.tag}>`;
+  }
+
+}
+
 // Export class here
 module.exports = {};
