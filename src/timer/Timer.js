c5eeb90585490bd60bef9e06d7af66df76db7107
Shahab <shahab13@uw.edu>
2019-04-15 21:05:24 -0700

    week_1_final

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,18 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+
+      console.log(this.seconds);
+      this.seconds -= 1;
+    }, 1000);
+  }
 }
-
-Timer.prototype.start = function () {
-  const instance = this;
-  var timerInterval = setInterval(() => {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
-
 module.exports = Timer;
