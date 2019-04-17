426e4dd5e06baec05dfb6f048198a0e30a9c100f
Katrinagd <katrinaguyduy@gmail.com>
2019-04-17 10:38:27 -0700

    fixed indentation to pass test

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -2,11 +2,11 @@ const HTMLElement = require('./HTMLElement');
 
 // Define class here
 class DivElement extends HTMLElement {
-    constructor(content) {
-      super('div', content)
-      this.content = content;
-    }
+  constructor(content) {
+    super('div', content);
+    this.content = content;
   }
+}
 const andIThinkToMyself = new DivElement('What a wonderful world');
 
 console.log(andIThinkToMyself.render());
--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,16 @@
 class Timer {
   constructor(seconds) {
     this.seconds = seconds;
-}
+  }
 
-start() {
-  const timerInterval = setInterval((instance) => {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
 
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
+      this.seconds -= 1;
+    }, 1000);
+  }
 }
 module.exports = Timer;
