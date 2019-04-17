3e7fa2887269d7a2170bc2dcaef9aea5a7cce393
Jeremyjdz13 <jeremyjdz@gmail.com>
2019-04-17 11:21:45 -0700

    Week 1 Homwork

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,15 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(text) {
+    super('div');
+    this.text = text;
+  }
+}
 
+const shakespear = new DivElement('To be or not to be?');
+
+console.log(shakespear.render());
 // Export class here
-module.exports = {};
+module.exports = DivElement;
--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,19 @@
 // Define class here
+// Define class here
+class HTMLElement {
+  constructor(tag, text) {
+    this.tag = tag;
+    this.text = text;
+  }
+
+  render() {
+    return `<${this.tag}>${this.text}</${this.tag}>`;
+  }
+}
+
+const h1 = new HTMLElement('h1', 'Web Design');
+
+console.log(h1.render());
 
 // Export class here
-module.exports = {};
+module.exports = HTMLElement;
