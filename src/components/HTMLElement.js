0098c242813d7eac0e6b2660095031357e9706ae
toan-myGitHub <47186111+toan-myGitHub@users.noreply.github.com>
2019-04-17 23:37:02 -0700

    Update HTMLElement.js

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,21 @@
 // Define class here
+class HTMLElement {
+    constructor(tag, content) {
+        this.tag = tag;
+        this.content = content;
+    }
+    render () {
+        return `<${this.tag}>${this.content}</${this.tag}>`;
+    }
+
+}
+
+const lovelaceQuote = new HTMLElement('p', 'I am never so happy as when I am really engaged in good earnest...');
+console.log(lovelaceQuote.render());
+//<p>I am never so happy as when I am really engaged in good earnest...</p>
+const div = new HTMLElement('div', 'University of Washington');
+console.log(div.render());
+//<div>University of Washington</div>
 
 // Export class here
-module.exports = {};
+module.exports = HTMLElement;
