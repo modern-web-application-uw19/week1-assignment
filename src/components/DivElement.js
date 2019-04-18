8c2a964352d1a37a4951e754e7899541bdd17313
toan-myGitHub <47186111+toan-myGitHub@users.noreply.github.com>
2019-04-17 23:37:39 -0700

    Update DivElement.js

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,16 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
+class DivElement extends HTMLElement {
+    constructor(content) {
+    super('div', content);
+    super.render(); 
+    }   
+} 
 
+const andIThinkToMyself = new DivElement('What a wonderful world');
+console.log(andIThinkToMyself.render());
+// prints "<div>What a wonderful world</div>"
+  
 // Export class here
 module.exports = {};
