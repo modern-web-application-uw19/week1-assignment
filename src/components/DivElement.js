8deb0626a2c0f760b6212e4f9f1a97130cc76c3c
Michael S. Hoffman <mikeshoffman@gmail.com>
2019-04-17 23:53:04 -0700

    Do problems 1 and 2

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,23 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
+const DivElement = function(content){
+    this.tag = tag;
+    this.content = content;
+}
+
+DivElement.prototype = Object.create(HTMLElement.prototype);
+DivElement.prototype.constructor = DivElement;
+
+DivElement.prototype.render = function(){
+    return this;
+}
+
+// usage:
+const andIThinkToMyself = new DivElement('What a wonderful world');
+
+console.log(andIThinkToMyself.render());
+// prints "<div>What a wonderful world</div>"
 
 // Export class here
 module.exports = {};
--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,20 @@
 // Define class here
 
+const HTMLElement = function(tag, content){
+    this.tag = tag;
+    this.content = content;
+}
+
+HTMLElement.prototype.render = function(){
+    return this;
+}
+
+// usage:
+const lovelaceQuote = new HTMLElement('p', 'I am never so happy as when I am really engaged in good earnest...');
+
+console.log(lovelaveQuote.render());
+// prints "<p>I am never so happy as when I am really engaged in good earnest...</p>"
+
 // Export class here
-module.exports = {};
+module.exports = HTMLElement;
+
