a460bb26129155e7626e706b19bcb08db5dc4bfe
Shawn Hopkins <shopkins@seagen.com>
2019-04-17 19:01:56 -0700

    attempt 1

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,10 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super('div', content);
+    this.content = content;
+  }
+}
 
-// Export class here
-module.exports = {};
