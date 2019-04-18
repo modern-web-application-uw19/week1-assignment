71c77c0c86501657a568e2c52239e721495c60f6
toan-myGitHub <47186111+toan-myGitHub@users.noreply.github.com>
2019-04-17 23:52:13 -0700

    Update Timer.js

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,41 +1,22 @@
-
-/*
-function Timer(seconds) {
-  this.seconds = seconds;
-}
-
-Timer.prototype.start = function() {
-  var instance = this;
-  var timerInterval = setInterval(function() {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
-*/
-
-class Timer {
+class Timer{
   constructor(seconds) {
     this.seconds = seconds;
-}
-getTime() {
-  return this.seconds;
-}
-setTime(value) {
-  this.seconds = value;
-}
-start () {  
-  var seconds = this.getTime();
-  var timerInterval = setInterval(function() {
+  }
+  getTime(){
+    return this.seconds;
+  }
+  setTime(value){
+    this.seconds = value;
+  }
+  start(){
+    var seconds = this.getTime();
+    var timerInterval = setInterval(function() {
     if (seconds === 0) {
       clearInterval(timerInterval);
     }
     console.log(seconds);
     seconds -= 1;
-  }, 1000);
-}
+    }, 1000);
+    }
 }
 module.exports = Timer;
