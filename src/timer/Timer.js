7eb5041d828fde8947c246c8fe73cffb38d82da4
scl9087 <49742564+scl9087@users.noreply.github.com>
2019-04-18 22:02:37 -0700

    class 1 assignment

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,22 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+/* Examine the code in these two files and determine each script's behavior.
+Refactor the code to use the ES6 class syntax.
+Refactor the code to avoid assigning this to a variable.
+ */
+
+class Timer {  //constructor function 
+  constructor(seconds) {
+    this.seconds = seconds;  //instance variable 
+  }
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+  
+      console.log(this.seconds);
+      this.seconds -= 1; //countdown seconds until value reaches 0, then clearInterval
+    }, 1000); //1 second intervals
+  }
 }
 
-Timer.prototype.start = function() {
-  var instance = this;
-  var timerInterval = setInterval(function() {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
-
-module.exports = Timer;
+module.exports = Timer; //exports Timer constructor
