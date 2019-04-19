b519af454640e6ecfb0b976ae5615453e170ab30
scl9087 <49742564+scl9087@users.noreply.github.com>
2019-04-18 22:17:22 -0700

    remove comments and spaces

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,11 +1,6 @@
-/* Examine the code in these two files and determine each script's behavior.
-Refactor the code to use the ES6 class syntax.
-Refactor the code to avoid assigning this to a variable.
- */
-
-class Timer {  //constructor function 
+class Timer {
   constructor(seconds) {
-    this.seconds = seconds;  //instance variable 
+    this.seconds = seconds;
   }
   start() {
     const timerInterval = setInterval(() => {
@@ -14,9 +9,9 @@ class Timer {  //constructor function
       }
   
       console.log(this.seconds);
-      this.seconds -= 1; //countdown seconds until value reaches 0, then clearInterval
-    }, 1000); //1 second intervals
+      this.seconds -= 1;
+    }, 1000);
   }
 }
 
-module.exports = Timer; //exports Timer constructor
+module.exports = Timer;
