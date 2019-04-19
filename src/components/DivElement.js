60c58e7737eae97b92f9ee23ea17e9a641f0b98e
Jana Uhrich <janamu@uw.edu>
2019-04-18 21:26:51 -0700

    Week 1 Homework, ES6 Classes

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,17 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super();
+    this.tag = 'div';
+    this.content = content;
+  }
+}
+
+// test
+const div = new DivElement('University of Washington');
+console.log(div.render());
 
 // Export class here
-module.exports = {};
+module.exports = DivElement;
