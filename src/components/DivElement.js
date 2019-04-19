c5a93848f27dd897a8f55003b8692033ab4de618
scl9087 <49742564+scl9087@users.noreply.github.com>
2019-04-18 22:12:18 -0700

    Update DivElement.js
    indentation fix

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -2,9 +2,9 @@ const HTMLElement = require('./HTMLElement');
 
 // Define class here
 class DivElement extends HTMLElement {
-    constructor(content) {
-        super('div', content);
-    }
+  constructor(content) {
+    super('div', content);
+  }
 }
 // Export class here
 module.exports = DivElement;
